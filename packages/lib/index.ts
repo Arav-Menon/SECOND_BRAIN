7fa1730f1eec977f0b246573790b61546b654599
Arav menon <aravmenon.ak@gmail.com>
2025-08-04 18:08:19 +0530

    add card input validation zod schema

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -5,3 +5,8 @@ export const authValidation = z.object({
 	email: z.string().email(),
 	password: z.string().min(6).max(100),
 });
+
+export const cardInputValidation = z.object({
+	title : z.string().max(50),
+	description : z.string().max(1000),
+})
