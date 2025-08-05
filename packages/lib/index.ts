3acd6c7f64e33dad3dd5e928a505f3e917c7edc6
Arav menon <aravmenon.ak@gmail.com>
2025-08-05 22:03:02 +0530

    add room validation

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,17 +1,20 @@
-import { getTsBuildInfoEmitOutputFilePath } from 'typescript';
-import z from 'zod';
+import z from "zod";
 
 export const authValidation = z.object({
-	userName: z.string().min(3).max(10).optional(),
-	email: z.string().email(),
-	password: z.string().min(6).max(100),
+  userName: z.string().min(3).max(10).optional(),
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
 });
 
 export const cardInputValidation = z.object({
-	title : z.string().max(50),
-	description : z.string().max(1000),
-})
+  title: z.string().max(50),
+  description: z.string().max(1000),
+});
 
 export const tagValidation = z.object({
-	title : z.string().max(15)
-})
+  title: z.string().max(15),
+});
+
+export const roomValidation = z.object({
+  name: z.string().min(4).max(20)
+});
